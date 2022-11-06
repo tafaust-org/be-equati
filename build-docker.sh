07ab962803de7dd5f0ce8a567e37b1fad4724b79
Thomas Hesse <thomas.hesse@neoloan.de>
2022-11-06 22:21:23 +0100

    refactor(build-docker.sh): refactor image name

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-IMAGE_NAME=${1:-be-contract}
+IMAGE_NAME=${1:-be-game}
 IMAGE_VERSION=$(jq -r '.version' package.json)
 docker build -t "${IMAGE_NAME}:latest" .
 docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${IMAGE_VERSION}"
